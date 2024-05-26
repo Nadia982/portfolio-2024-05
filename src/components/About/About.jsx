ee841c3a3e338b8c9c0981d6d92e65559c825c1a
Natalie Gillam <ngillam@hotmail.co.uk>
2024-05-26 23:45:27 +0100

    added content of About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
 export const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className={styles.container}>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about /cursorIcon.png")} alt="cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building scalable
+                and optimised sites.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,10 +8,16 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Natalie</h1>
         <p className={styles.description}>
-          I'm a UK-based web developer with 2 years of experience using React, JavaScript and Python. I also provide remote Computer Science tuitions to GCSE students. Drop me an email if you'd like to get in touch!
+          I'm a UK-based web developer with 2 years of experience using React,
+          JavaScript and Python. I also provide remote Computer Science tuitions
+          to GCSE students.
+        </p>
+        <p className={styles.description}>
+          Drop me an email or send me a message on LinkedIn if you'd like to get
+          in touch!
         </p>
         <a href="#" className={styles.contactBtn}>
-        {/* <a href="mailto:myemail@email.com" className={styles.contactBtn}> */}
+          {/* <a href="mailto:myemail@email.com" className={styles.contactBtn}> */}
           Contact Me
         </a>
       </div>
