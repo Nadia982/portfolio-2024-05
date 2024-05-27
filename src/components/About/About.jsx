1e75e7d1b51c406676a6a8bad4ceeaf1d849a635
Natalie Gillam <ngillam@hotmail.co.uk>
2024-05-28 00:46:17 +0100

    added styling to About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,7 @@ import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
 export const About = () => {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         <img
@@ -13,7 +13,7 @@ export const About = () => {
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about /cursorIcon.png")} alt="cursor icon" />
+            <img src={getImageUrl("about/cursorIcon.png")} alt="cursor icon" />
             <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
               <p>
