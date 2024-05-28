2fdca3060380d03aa025b8f75919d687844744b3
Natalie Gillam <ngillam@hotmail.co.uk>
2024-05-28 02:03:50 +0100

    updated About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,26 +4,42 @@ import { getImageUrl } from "../../utils";
 export const About = () => {
   return (
     <section className={styles.container} id="about">
-      <h2 className={styles.title}>About</h2>
-      <div className={styles.content}>
+      <h2 className={styles.title}>About Me</h2>
+      {/* <div className={styles.content}>
         <img
           src={getImageUrl("about/aboutImage.png")}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
-        />
+        /> */}
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="cursor icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
-              <p>
-                I'm a frontend developer with experience in building scalable
-                and optimised sites.
+              
+              <p className={styles.aboutItemPara}>
+                I have worked in the tech industry for the past 12 years, most
+                recently as a Senior Paralegal at a global IT services company
+                headquartered in London, where I work with a global remotely
+                distributed team, and have helped the business to expand its
+                operations into 24 new countries spread across five continents.
+              </p>
+              <p className={styles.aboutItemPara}>
+                In 2021, I taught myself to code, and since then, I have used my
+                coding skills to build applications that benefit the students I
+                teach Maths and Computer Science to in my spare time.
+              </p>
+              <p className={styles.aboutItemPara}>
+                My real passion and professional focus is now frontend web
+                development. I am looking for a role as a React Developer that
+                will enable me to learn as much as possible about new and
+                existing technologies while building apps and sites to help your
+                business flourish. I enjoy collaborating with and learning from
+                others while sharing my knowledge and contributing to a team.
               </p>
             </div>
           </li>
         </ul>
-      </div>
+      {/* </div> */}
     </section>
   );
 };
