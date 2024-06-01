bfc79878a7d4ee5c9ae3877c0872e32b406a9099
Natalie Gillam <ngillam@hotmail.co.uk>
2024-06-01 14:54:58 +0100

    amended content of Experience section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Natalie</h1>
         <p className={styles.description}>
-          I'm a <span className={styles.emphasis}>UK-based</span> web developer with <span className={styles.emphasis}>2&nbsp;years</span> of experience using <span className={styles.emphasis}>React</span>, <span className={styles.emphasis}>JavaScript</span> & <span className={styles.emphasis}>Python</span>. I also tutor Computer&nbsp;Science & Maths online.
+          I'm a <span className={styles.emphasis}>UK-based</span> web developer with <span className={styles.emphasis}>3&nbsp;years</span> of experience using <span className={styles.emphasis}>React</span>, <span className={styles.emphasis}>JavaScript</span> & <span className={styles.emphasis}>Python</span>. I also tutor Computer&nbsp;Science & Maths online.
         </p>
         <p className={styles.description}>
           Drop me an email or a LinkedIn message if you'd like to get
