062658b937cb6e1d553af3b285afac56b5516348
Natalie Gillam <ngillam@hotmail.co.uk>
2024-06-02 00:41:48 +0100

    added styles in Projects.module.css

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,34 +1,24 @@
 import React from "react";
+import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
-// {
-//     "id":"01",
-//     "category": ["javascript", "react"],
-//     "title": "Grammar Quiz for Year 6 Kids",
-//     "imgUrl": "projects/portfolioImg1.jpg",
-//     "description": "I created this project to fulfil a need for my child to practice answering grammar-related questions before his SATS (UK school standard assessment tests for 10-11 year-olds). Multiple choice answers and animations that give immediate feedback on responses make this quiz easy and compelling to use. ",
-//     "skills": ["JavaScript", "HTML"],
-//     "siteUrl": "",
-//     "gitHub": "",
-//     "alt": "A screenshot of project 1: a multiple choice quiz about English grammar on a dark blue background with four possible answers. At the bottom of the screen there are answer indicators indicating whether previous questions have been answered correctly."
-//     },
 
 export const ProjectCard = ({
   project: { title, imgUrl, description, skills, siteUrl, gitHub, alt },
 }) => {
   return (
-    <div>
-      <img src={getImageUrl(imgUrl)} alt={alt} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <ul>
+    <div className={styles.container}>
+      <img src={getImageUrl(imgUrl)} alt={alt} className={styles.image}/>
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.skills}>
         {skills.map((skill, id) => {
-          <li key={id}>{skill}</li>;
+          <li className={styles.skill} key={id}>{skill}</li>;
           1;
         })}
       </ul>
-      <div>
-        <a href={siteUrl}>Live</a>
-        <a href={gitHub}>Code</a>
+      <div className={styles.links}>
+        <a href={siteUrl} className={styles.link}>Live</a>
+        <a href={gitHub} className={styles.link}>Code</a>
       </div>
     </div>
   );
