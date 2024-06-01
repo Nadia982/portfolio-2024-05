c33f0e3592dbfe4f09ce4074a5f2dc5d23cc52bf
Natalie Gillam <ngillam@hotmail.co.uk>
2024-06-01 23:47:20 +0100

    updated text of About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -20,19 +20,18 @@ export const About = () => {
               <p className={styles.aboutItemPara}>
                 I have worked in the tech industry for the past 12 years, most
                 recently as a Senior Paralegal at a global IT services company
-                headquartered in London.
+                headquartered in London. I also tutor Maths and Computer Science online to GCSE students, having studied Maths to a high level previously.
               </p>
               
               <p className={styles.aboutItemPara}>
                 In 2021, I taught myself to code, and since then, I have used my
                 coding skills to build applications that benefit the students I
-                teach Maths and Computer Science to in my spare time.
+                tutor. My real passion and professional focus is now frontend web
+                development. 
               </p>
               <p className={styles.aboutItemPara}>
-                My real passion and professional focus is now frontend web
-                development. I am looking for a role as a React Developer that
-                will enable me to learn as much as possible about new and
-                existing technologies while building apps and sites to help your
+                I am looking for a role as a React Developer that
+                will enable me to indulge in my love of ongoing learning, while building apps and sites to help your
                 business flourish. I enjoy collaborating with and learning from
                 others while sharing my knowledge and contributing to a team.
               </p>
