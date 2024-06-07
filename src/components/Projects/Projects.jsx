a3060d24b8f88d2d95db3556da45f3e4eb59ea48
Natalie Gillam <ngillam@hotmail.co.uk>
2024-06-07 23:53:54 +0100

    added 'overflow: hidden' to App.module.css

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -53,7 +53,7 @@ export const Projects = () => {
       <h2 className={styles.title}>Projects</h2>
     
 <div className={styles.buttonContainer}>
-<p className={styles.filterBySkill}>Filter projects by skill:</p>
+<p className={styles.filterBySkill}>Filter projects<br/> by skill:</p>
       {/* <button className={`${styles.filterButton} ${selectTab && styles.selected}`} onClick={() => setSelectTab("all")}>All</button> */}
       <button className={`${styles.filterButton}`} onClick={() => setSelectTab("all")}>All</button>
 
